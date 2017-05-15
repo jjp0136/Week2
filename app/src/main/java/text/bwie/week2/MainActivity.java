fe60b4ab294d0179d6a80b5dbf1215a40e45c2e6
jjp0136 <qwqa5210>
2017-05-15 20:15:31 +0800

    xiangguo

--- app/src/main/java/text/bwie/week2/MainActivity.java
+++ app/src/main/java/text/bwie/week2/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         lv = (ListView) findViewById(R.id.lv);
         getInfo();
-
+//sssss
     }
 
     private void getInfo() {
